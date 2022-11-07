a89b54cbe7c56802e5c02ebfa913346d300c8dc4
Andrew Gay <58545642+andrwmg@users.noreply.github.com>
2022-11-06 19:21:36 -0800

    Update to React Resume

--- src/Resume.js
+++ src/Resume.js
@@ -112,7 +112,19 @@ const Resume = () => {
                         <div className="col-6 col-sm-6 col-md-6 text-primary my-auto">
                             <div className="progress" style={{ height: 10 }}>
                                 <div className="progress-bar" role="progressbar" aria-label="Example 20px high"
-                                    style={{ width: '60%' }} aria-valuenow="60" aria-valuemin="0" aria-valuemax="100">
+                                    style={{ width: '80%' }} aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row mb-2 justify-content-start justify-content-sm-center justify-content-md-start">
+                        <div className="col-6 col-sm-6 col-md-6">
+                            <h5>MUI</h5>
+                        </div>
+                        <div className="col-6 col-sm-6 col-md-6 text-primary my-auto">
+                            <div className="progress" style={{ height: 10 }}>
+                                <div className="progress-bar" role="progressbar" aria-label="Example 20px high"
+                                    style={{ width: '80%' }} aria-valuenow="80" aria-valuemin="0" aria-valuemax="100">
                                 </div>
                             </div>
                         </div>
