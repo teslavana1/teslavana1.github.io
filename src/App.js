ec389d1ef4513c3cfec04846394fb8d586e71519
Andrew Gay <58545642+andrwmg@users.noreply.github.com>
2022-11-20 11:53:02 -0800

    add teslamart v2

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Portfolio from './Portfolio';
 import Nav from './Nav';
 import { Route, Routes, Navigate } from 'react-router-dom';
 import UseRedirectToHttps from './hooks/useRedirectToHttps';
+// import Navbar from './Navbar';
 
 
 function App() {
--- src/PortfolioSeeds.js
+++ src/PortfolioSeeds.js
@@ -1,9 +1,15 @@
 const websites = [
     {
+        name: 'Tesla Mart V2',
+        url: 'https://teslamartv2.herokuapp.com',
+        image: 'https://res.cloudinary.com/deuft4auk/image/upload/v1668972561/Tesla/Screenshot_2022-11-19_at_10.54.18_PM_ps8stc.png',
+        description: "After completing my React bootcamp, I rebuilt Tesla Mart from scratch to solidify my new skills. I found it much easier to create a polished, stable website using React and MUI. The use of state made storing data from API REST requests using Axios a breeze, and breaking the website down into components made the code easier to follow and follow. Using the useContext hook helped keep the functionality contained. I would still like to add more features, but I am very happy with the progress so far."
+    },
+    {
         name: 'Tesla Mart',
         url: 'https://teslamart.herokuapp.com',
         image: 'https://res.cloudinary.com/deuft4auk/image/upload/v1666389702/Tesla/Screen_Shot_2022-10-20_at_11.26.07_PM_csbrtt.jpg',
-        description: "Tesla Mart is a CRUD web app  where current and future Tesla owners can buy and sell their vehicles.  The layout was built using HTML, CSS, Bootstrap, and JavaScript, while the back-end was built using the REST architecture with NodeJS, Express, and MongoDB.  Every page of the website is fully responsive.  The filtering and sorting functionality was difficult to implement, but it is now flawless. The filter dropdowns update depending on which other filters are active."
+        description: "Using the skills I learned from my first web development bootcamp, I built Tesla Mart. Tesla Mart is a CRUD web app  where current and future Tesla owners can buy and sell their vehicles.  The layout was built using HTML, CSS, Bootstrap, and JavaScript, while the back-end was built using the REST architecture with NodeJS, Express, and MongoDB.  Every page of the website is fully responsive.  The filtering and sorting functionality was difficult to implement, but it is now flawless. The filter dropdowns update depending on which other filters are active."
     }
 ]
 
