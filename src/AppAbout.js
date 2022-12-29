bbe2c4846c3ba2f2e1f28a9b58c1089a06c2c2f9
andrwmg <58545642+andrwmg@users.noreply.github.com>
2022-12-28 23:38:29 -0800

    Switch to MUI and add detail page

--- src/AppAbout.js
+++ src/AppAbout.js
@@ -31,12 +31,12 @@ export default function AppAbout() {
                 return (
                     (index % 2 === 0) ?
 
-                        <Grid key={seed.heading} display={index === 0 && 'none'} container item direction='row' height={{ md: '100%' }} position='relative' justifyContent='flex-start' bgcolor={seed.background}>
+                        <Grid key={seed.heading} display={index === 0 && 'none'} container item direction='row' height={{xs: 'auto', md: '100%' }} position='relative' justifyContent='flex-start' bgcolor={seed.background}>
                             <AppAboutImage imageUrl={seed.imageUrl} side='25%' />
                             <AppAboutText heading={seed.heading} text={seed.text} color={seed.color} />
                         </Grid>
                         :
-                        <Grid key={seed.heading} container item direction='row' flexWrap='wrap-reverse' height={{ md: '100%' }} position='relative' justifyContent='flex-end' alignItems='center' bgcolor={seed.background}>
+                        <Grid key={seed.heading} container item direction='row' flexWrap='wrap-reverse' height={{xs: 'auto', md: '100%' }} position='relative' justifyContent='flex-end' alignItems='center' bgcolor={seed.background}>
                             <AppAboutText heading={seed.heading} text={seed.text} color={seed.color}  />
                             <AppAboutImage imageUrl={seed.imageUrl} side='-25%' />
                         </Grid>
--- src/AppAboutText.js
+++ src/AppAboutText.js
@@ -3,12 +3,12 @@ import React from "react";
 
 export default function AppAboutText({ heading, text, color }) {
     return (
-        <Grid item container direction='column' xs={12} md={6} py={2} justifyContent='center' position={{md: 'absolute' }} height={{xs: 'auto', md: 'calc(100vh - 56px)' }} px={{xs: 2, md: 6}} zIndex={10}>
+        <Grid item container direction='column' xs={12} md={6} py={2} justifyContent='center' position={{md: 'absolute' }} height={{xs: 'auto', md: 'calc(100vh - 56px)' }} px={{xs: 2, md: 6}} zIndex={10} wrap='nowrap'>
             <Typography variant="h4" color={color} marginBottom={3}>
                 <b>{heading}</b>
             </Typography>
             {text.map(p => (
-                <Typography position='' key={p} variant="body1" color='text.primary' mb={2}>
+                <Typography key={p} variant="body1" color='text.primary' mb={2}>
                     {p}
                 </Typography>
             ))}
