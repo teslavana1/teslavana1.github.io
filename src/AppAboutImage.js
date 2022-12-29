1c2222cb7f9cb43e884fad1fcdebf02ad3213298
andrwmg <58545642+andrwmg@users.noreply.github.com>
2022-12-29 13:48:43 -0800

    Switch to MUI and add detail page

--- src/AppAboutImage.js
+++ src/AppAboutImage.js
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function AppAboutImage ({imageUrl, side}) {
     return(
-        <Grid item container justifyContent='center' alignItems='center' height='calc(100vh - 56px)' overflow='hidden'>
+        <Grid item container justifyContent='center' alignItems='center' minHeight='calc(100vh - 56px)' height={{xs:'calc(100vh - 56px)', md:'100%', lg: 'calc(100vh - 56px)'}} overflow='hidden'>
         <img src={imageUrl} alt='' height='100%' width='100%' style={{objectFit: 'cover', translate: side}} />
         </Grid>
     )
--- src/AppAboutText.js
+++ src/AppAboutText.js
@@ -3,7 +3,7 @@ import React from "react";
 
 export default function AppAboutText({ heading, text, color }) {
     return (
-        <Grid item container direction='column' xs={12} md={6} py={2} justifyContent='center' position={{md: 'absolute' }} height={{xs: 'auto', md: 'calc(100vh - 56px)' }} px={{xs: 2, md: 6}} zIndex={10} wrap='nowrap'>
+        <Grid item container direction='column' xs={12} md={6} py={2} justifyContent='center' position={{md: 'absolute' }} height={{xs: 'auto', md: '100%' }} px={{xs: 2, md: 6}} zIndex={10} wrap='nowrap'>
             <Typography variant="h4" color={color} marginBottom={3}>
                 <b>{heading}</b>
             </Typography>
