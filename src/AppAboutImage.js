a0314eceb8cbe61589ed7059ba9378ff51645046
andrwmg <58545642+andrwmg@users.noreply.github.com>
2023-01-03 19:40:01 -0800

    Use absolute positioning instead of translation on details images for larger screen sizes

--- src/AppAboutImage.js
+++ src/AppAboutImage.js
@@ -3,9 +3,10 @@ import React from "react";
 
 export default function AppAboutImage ({imageUrl, side}) {
     return(
-        <Grid item container justifyContent='center' alignItems='center' minHeight='calc(100vh - 56px)' height={{xs:'calc(100vh - 56px)', md:'100%', lg: 'calc(100vh - 56px)'}} overflow='hidden'>
+        <Grid item container justifyContent='center' alignItems='center' minHeight='calc(100vh - 56px)' height={{xs:'calc(100vh - 56px)', md:'100%', lg: 'calc(100vh - 56px)'}} overflow='hidden' sx={{translate: {xs: side}}}>
         <img src={imageUrl} alt='' height='100%' width='100%' style={{objectFit: 'cover', 
-        translate: side
+        position: {md: 'absolute'}, right: {md: side},
+        // translate: {xs: side, md: null}
         }} />
         </Grid>
     )
