a37ca06fc41213d684d5c91d576b50e96e460e26
andrwmg <58545642+andrwmg@users.noreply.github.com>
2023-02-21 21:17:42 -0800

    First mech push

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import "./App.css";
 import Resume from "./ResumeMui";
-import Portfolio from "./Portfolio";
 import { Route, Routes, Navigate } from "react-router-dom";
-import UseRedirectToHttps from "./hooks/useRedirectToHttps";
-import AppAbout from "./AppAbout";
 import DrawerAppBar from "./Navbar";
 
 function App() {
@@ -13,6 +10,7 @@ function App() {
       <DrawerAppBar>
         <Routes>
           <Route path="/" element={<Resume />} />
+          <Route path="*" element={<Navigate replace to="/" />} />
         </Routes>
       </DrawerAppBar>
     </div>
